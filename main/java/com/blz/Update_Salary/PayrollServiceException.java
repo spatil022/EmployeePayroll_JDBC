745b5479c126a6a1e533472d33f41db76fed83e2
spatil022 <shamalpatil1998@gmail.com>
2021-03-17 13:47:08 +0530

    added

---
+++ main/java/com/blz/Update_Salary/PayrollServiceException.java
@@ -0,0 +1,13 @@
+package com.blz.Update_Salary;
+
+public class PayrollServiceException extends Exception{
+	enum ExceptionType{
+		CONNECTION_PROBLEM, RETRIEVAL_PROBLEM, UPDATE_PROBLEM; 
+	}
+
+	ExceptionType type;
+	PayrollServiceException(String message, ExceptionType type){
+		super(message);
+		this.type = type;
+	}
+}
