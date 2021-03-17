75bf9adee8b4b96cd8c05a74ab6568f2b3d5700e
spatil022 <shamalpatil1998@gmail.com>
2021-03-17 13:53:32 +0530

    added

---
+++ main/java/com/blz/AverageSalary/PayrollServiceException.java
@@ -0,0 +1,14 @@
+package com.blz.AverageSalary;
+
+
+public class PayrollServiceException extends Exception{
+	enum ExceptionType{
+		CONNECTION_PROBLEM, RETRIEVAL_PROBLEM, UPDATE_PROBLEM; 
+	}
+
+	ExceptionType type;
+	PayrollServiceException(String message, ExceptionType type){
+		super(message);
+		this.type = type;
+	}
+}
